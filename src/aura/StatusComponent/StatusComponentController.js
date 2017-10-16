05736506fd06192e4683b9bdf15c8be84e7b6fd3
artem-melnikov <artem.melnikov270595@gmail.com>
2017-10-16 18:03:02 +0300

    added classes: AggregationResponse, AggregationSetting changed classes: StatusComponentService

--- src/aura/StatusComponent/StatusComponentController.js
+++ src/aura/StatusComponent/StatusComponentController.js
@@ -5,6 +5,8 @@
       doInit: function(component, event, helper) {
           console.log('updated4');
           var action = component.get("c.request");
+          //
+          //getting related records
           var json = {
                 className: 'StatusComponentController',
                 methodName: 'getRelatedRecords',
@@ -20,13 +22,39 @@
               if (state === "SUCCESS") {
                   //объект полей
                   var responseJSON = JSON.parse(response.getReturnValue());
-                  console.log('responseJSON', responseJSON.result);
-                  component.set('v.response', responseJSON.result);
+                  console.log('relatedJSON', responseJSON.result);
+                  component.set('v.relatedRecords', responseJSON.result);
               }
               else {
                   console.log("Failed with state: " + state);
               }
           });
           $A.enqueueAction(action);
+          //
+          //getting aggregation records
+          action = component.get("c.request");
+          json = {
+                  className: 'StatusComponentController',
+                  methodName: 'getAggregationRecords',
+                  parameters: component.get("v.recordId").toString()
+           };
+          jsonString = JSON.stringify(json);
+          console.log('jsonString: ', jsonString)
+          action.setParams({
+            "requestString": jsonString
+          });
+          action.setCallback(this, function(response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    //объект полей
+                    var responseJSON = JSON.parse(response.getReturnValue());
+                    console.log('aggregationJSON', responseJSON.result);
+                    component.set('v.aggregationRecords', responseJSON.result);
+                }
+                else {
+                    console.log("Failed with state: " + state);
+                }
+            });
+        $A.enqueueAction(action);
       }
   })
